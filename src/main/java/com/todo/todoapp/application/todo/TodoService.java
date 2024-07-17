81f2d9b897044920aa5e631e3fccae30af9745a1
megaseunghan <84169773+megaseunghan@users.noreply.github.com>
2024-07-17 18:38:46 +0900

    :sparkles: TodoService 할 일 수정 기능 추가
    - 기존 Todo 단건 조회 기능 로직 중복으로 인해 메서드(getTodoById())로 추출

--- src/main/java/com/todo/todoapp/application/todo/TodoService.java
+++ src/main/java/com/todo/todoapp/application/todo/TodoService.java
@@ -3,6 +3,7 @@ package com.todo.todoapp.application.todo;
 import com.todo.todoapp.domain.todo.model.Todo;
 import com.todo.todoapp.domain.todo.repository.TodoRepository;
 import com.todo.todoapp.presentation.todo.dto.request.CreateTodoRequest;
+import com.todo.todoapp.presentation.todo.dto.request.UpdateTodoRequest;
 import com.todo.todoapp.presentation.todo.dto.response.TodoResponse;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -23,8 +24,7 @@ public class TodoService {
     }
 
     public TodoResponse find(long id) {
-        Todo todo = todoRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException(id + "로 조회되는 할 일은 없습니다"));
+        Todo todo = getTodoById(id);
         return TodoResponse.from(todo);
     }
 
@@ -33,4 +33,24 @@ public class TodoService {
         todos.sort(Comparator.comparing(Todo::getCreatedDate).reversed());
         return todos.stream().map((TodoResponse::from)).toList();
     }
+
+    @Transactional
+    public TodoResponse update(long id, UpdateTodoRequest request) {
+        Todo todo = getTodoById(id);
+
+        if (request.password() != todo.getPassword()) {
+            throw new IllegalArgumentException("비밀번호가 올바르지 않습니다.");
+        }
+
+        todo.update(request);
+
+        return TodoResponse.from(todo);
+    }
+
+    private Todo getTodoById(long id) {
+        Todo todo = todoRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException(id + "로 조회되는 할 일은 없습니다"));
+        return todo;
+    }
+
 }
