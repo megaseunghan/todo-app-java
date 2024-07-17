e97a0b904424a96aa4b2979512727d2205a4b7b4
megaseunghan <84169773+megaseunghan@users.noreply.github.com>
2024-07-17 23:12:31 +0900

    :sparkles: REST API 전역 예외 핸들링을 위한 RestApiExceptoinHnadler 추가

---
+++ src/main/java/com/todo/todoapp/global/exception/handler/RestApiExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.todo.todoapp.global.exception.handler;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RestApiExceptionHandler {
+
+}
