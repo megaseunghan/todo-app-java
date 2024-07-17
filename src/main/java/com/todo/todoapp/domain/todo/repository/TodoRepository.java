31f6573b1f45728cab7a8f41fcf3023630c1e3af
megaseunghan <84169773+megaseunghan@users.noreply.github.com>
2024-07-17 18:01:11 +0900

    :sparkles: TodoRepository 할 일 단건 / 목록 조회 기능 추가

--- src/main/java/com/todo/todoapp/domain/todo/repository/TodoRepository.java
+++ src/main/java/com/todo/todoapp/domain/todo/repository/TodoRepository.java
@@ -2,6 +2,13 @@ package com.todo.todoapp.domain.todo.repository;
 
 import com.todo.todoapp.domain.todo.model.Todo;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TodoRepository {
     Todo save(Todo todo);
+
+    Optional<Todo> findById(long id);
+
+    List<Todo> findAll();
 }
