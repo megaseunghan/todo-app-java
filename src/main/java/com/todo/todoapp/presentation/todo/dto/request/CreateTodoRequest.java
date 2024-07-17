146ee4e9618de64aaee2f827fadfccf368a71e4a
megaseunghan <84169773+megaseunghan@users.noreply.github.com>
2024-07-18 01:07:02 +0900

    :sparkles: Request DTO 유효성 검사 추가

--- src/main/java/com/todo/todoapp/presentation/todo/dto/request/CreateTodoRequest.java
+++ src/main/java/com/todo/todoapp/presentation/todo/dto/request/CreateTodoRequest.java
@@ -1,7 +1,8 @@
 package com.todo.todoapp.presentation.todo.dto.request;
 
 import com.todo.todoapp.domain.todo.model.Todo;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
@@ -9,13 +10,17 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 public record CreateTodoRequest(
-        @NotNull(message = "제목을 입력해주세요")
+
+        @Size(max = 200)
+        @NotBlank(message = "제목을 입력해주세요")
         String title,
-        @NotNull(message = "내용을 입력해주세요")
+        @Size(max = 200)
+        @NotBlank(message = "내용을 입력해주세요")
         String description,
-        @NotNull(message = "담당자를 입력해주세요")
+        @Email(regexp = "[a-z0-9]+@[a-z]+\\.[a-z]{2,3}", message = "올바른 이메일 형식을 입력해주세요")
+        @NotBlank(message = "담당자를 입력해주세요")
         String manager,
-        @NotNull(message = "비밀번호를 입력해주세요(4글자")
+        @NotBlank(message = "비밀번호는 필수 입력입니다.(4글자)")
         @Size(min = 4, max = 4)
         String password,
 
