c359f514e2f0bfa7bf34912c1d71e7d60212474b
megaseunghan <84169773+megaseunghan@users.noreply.github.com>
2024-07-18 00:43:56 +0900

    :sparkles: CommonErrorCode 알 수 없는 서버 에러 Enum 필드 추가

--- src/main/java/com/todo/todoapp/global/exception/common/code/CommonErrorCode.java
+++ src/main/java/com/todo/todoapp/global/exception/common/code/CommonErrorCode.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum CommonErrorCode implements ErrorCode {
-    INVALID_ARGS(HttpStatus.BAD_REQUEST, "유효성 검증 실패. 양식에 맞는 값이 아닙니다.");
+    INVALID_ARGS(HttpStatus.BAD_REQUEST, "유효성 검증 실패. 양식에 맞는 값이 아닙니다."),
+    UNKNOWN_ERR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 에러가 발생했습니다."),
+    ;
 
     private HttpStatus httpStatus;
     private String message;
