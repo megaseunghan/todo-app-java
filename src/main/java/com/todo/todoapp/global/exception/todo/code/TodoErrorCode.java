df18f96ff69460d2202ffcb73fde2e156278bfcc
megaseunghan <84169773+megaseunghan@users.noreply.github.com>
2024-07-18 00:35:49 +0900

    :recycle: TodoErrorCode Enum 중복 필드 삭제(ALREADY_DELETED)

--- src/main/java/com/todo/todoapp/global/exception/todo/code/TodoErrorCode.java
+++ src/main/java/com/todo/todoapp/global/exception/todo/code/TodoErrorCode.java
@@ -11,10 +11,8 @@ public enum TodoErrorCode implements ErrorCode {
 
     // 수정 / 삭제 시, 비밀번호가 일치하지 않을 때
     INCORRECT_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다."),
-    // 선택한 일정 정보가 이미 삭제되어 조회할 수 없을 때
-    FIND_FAILED(HttpStatus.NO_CONTENT, "선택하신 일정은 조회할 수 없습니다."),
-    // 삭제하려는 일정 정보가 이미 삭제 상태일 때
-    ALREADY_DELETED(HttpStatus.NOT_FOUND, "해당 일정은 존재하지 않거나 이미 삭제되었습니다.");
+    // 일정이 존재하지 않거나 이미 삭제 되었을 때
+    FIND_FAILED(HttpStatus.NO_CONTENT, "해당 일정은 존재하지 않거나 이미 삭제되었습니다."),
 
     private HttpStatus httpStatus;
     private String message;
