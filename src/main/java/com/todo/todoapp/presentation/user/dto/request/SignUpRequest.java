bf1eb38452b504ba0590b4b32b860c9b6fd52562
megaseunghan <84169773+megaseunghan@users.noreply.github.com>
2024-07-19 15:06:44 +0900

    :sparkles: 회원가입 요청 DTO 생성 & Role 생성

---
+++ src/main/java/com/todo/todoapp/presentation/user/dto/request/SignUpRequest.java
@@ -0,0 +1,17 @@
+package com.todo.todoapp.presentation.user.dto.request;
+
+import com.todo.todoapp.domain.user.vo.Role;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record SignUpRequest(
+        @NotBlank(message = "닉네임을 입력해주세요")
+        String nickname,
+
+        @NotBlank(message = "사용자 이름을 입력해주세요")
+        @Pattern(regexp = "^[a-z0-9]{4,10}$", message = "올바른 양식을 입력해주세요")
+        String userName,
+        String password,
+        Role role
+) {
+}
