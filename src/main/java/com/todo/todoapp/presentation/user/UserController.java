4fd33d8f73003b66496d5202598b809a9c4fbb92
megaseunghan <84169773+megaseunghan@users.noreply.github.com>
2024-07-19 14:32:17 +0900

    :sparkles: UserController 생성 및 회원가입 메서드 작성 완료

---
+++ src/main/java/com/todo/todoapp/presentation/user/UserController.java
@@ -0,0 +1,26 @@
+package com.todo.todoapp.presentation.user;
+
+import com.todo.todoapp.presentation.user.dto.request.SignUpRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<String> signup(@RequestBody SignUpRequest request) {
+        long id = userService.signup(request);
+        URI uri = URI.create(String.format("/users/%d", id));
+        return ResponseEntity.created(uri).body("회원가입 완료!");
+    }
+}
