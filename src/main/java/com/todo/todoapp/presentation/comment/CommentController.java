0663af66b7e4037dae5181e1efd3926bcaf2d716
megaseunghan <84169773+megaseunghan@users.noreply.github.com>
2024-07-29 22:33:35 +0900

    :sparkles: CommentController @Authenticate 추가

--- src/main/java/com/todo/todoapp/presentation/comment/CommentController.java
+++ src/main/java/com/todo/todoapp/presentation/comment/CommentController.java
@@ -1,9 +1,11 @@
 package com.todo.todoapp.presentation.comment;
 
 import com.todo.todoapp.application.comment.CommentService;
+import com.todo.todoapp.global.auth.annotation.Authenticate;
 import com.todo.todoapp.presentation.comment.dto.request.CreateCommentRequest;
 import com.todo.todoapp.presentation.comment.dto.request.UpdateCommentRequest;
 import com.todo.todoapp.presentation.comment.dto.response.CommentResponse;
+import com.todo.todoapp.presentation.user.dto.response.AuthenticateUser;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,9 +23,10 @@ public class CommentController {
     @PostMapping("/{todoId}")
     public ResponseEntity<CommentResponse> save(
             @PathVariable long todoId,
-            @Valid @RequestBody CreateCommentRequest request
+            @Valid @RequestBody CreateCommentRequest request,
+            @Authenticate AuthenticateUser user
     ) {
-        CommentResponse response = commentService.save(todoId, request);
+        CommentResponse response = commentService.save(todoId, request, user);
         long id = response.id();
         URI uri = URI.create(String.format("/comments/%d/%d", todoId, id));
         return ResponseEntity.created(uri).body(response);
@@ -32,15 +35,18 @@ public class CommentController {
     @PatchMapping("/{id}")
     public ResponseEntity<CommentResponse> update(
             @PathVariable long id,
-            @Valid @RequestBody UpdateCommentRequest request) {
-        CommentResponse response = commentService.update(id, request);
+            @Valid @RequestBody UpdateCommentRequest request,
+            @Authenticate AuthenticateUser user) {
+        CommentResponse response = commentService.update(id, request, user);
 
         return ResponseEntity.ok().body(response);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable long id) {
-        commentService.delete(id);
+    public ResponseEntity<Void> delete(
+            @PathVariable long id,
+            @Authenticate AuthenticateUser user) {
+        commentService.delete(id, user);
         return ResponseEntity.noContent().build();
 
     }
