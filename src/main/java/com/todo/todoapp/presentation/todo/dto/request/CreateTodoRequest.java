6fcbd9b59bcf735d1947ee04f86a9b88cf9a7d93
megaseunghan <84169773+megaseunghan@users.noreply.github.com>
2024-07-29 22:31:42 +0900

    :sparkles: TODO DTO 수정

--- src/main/java/com/todo/todoapp/presentation/todo/dto/request/CreateTodoRequest.java
+++ src/main/java/com/todo/todoapp/presentation/todo/dto/request/CreateTodoRequest.java
@@ -1,7 +1,7 @@
 package com.todo.todoapp.presentation.todo.dto.request;
 
 import com.todo.todoapp.domain.todo.model.Todo;
-import jakarta.validation.constraints.Email;
+import com.todo.todoapp.domain.user.model.User;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -12,20 +12,13 @@ public record CreateTodoRequest(
         String title,
         @Size(max = 200)
         @NotBlank(message = "내용을 입력해주세요")
-        String description,
-        @Email(regexp = "[a-z0-9]+@[a-z]+\\.[a-z]{2,3}", message = "올바른 이메일 형식을 입력해주세요")
-        @NotBlank(message = "담당자를 입력해주세요")
-        String manager,
-        @NotBlank(message = "비밀번호는 필수 입력입니다.(4글자)")
-        @Size(min = 4, max = 4)
-        String password
+        String description
 ) {
-    public Todo toEntity() {
+    public Todo toEntity(User user) {
         return Todo.builder()
                 .title(title)
                 .description(description)
-                .manager(manager)
-                .password(password)
+                .user(user)
                 .build();
     }
 }
