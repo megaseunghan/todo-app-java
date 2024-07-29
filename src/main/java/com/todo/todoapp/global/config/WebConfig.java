3a0de1547641c2edc63de30f5526e4e7559c065d
megaseunghan <84169773+megaseunghan@users.noreply.github.com>
2024-07-29 16:58:35 +0900

    :sparkles: 커스텀 어노테이션 및 ArgumentResolver 추가

--- src/main/java/com/todo/todoapp/global/config/WebConfig.java
+++ src/main/java/com/todo/todoapp/global/config/WebConfig.java
@@ -5,14 +5,24 @@ import com.todo.todoapp.application.user.UserService;
 import com.todo.todoapp.global.auth.filter.JwtAuthorizationFilter;
 import com.todo.todoapp.global.auth.filter.JwtLoginFilter;
 import com.todo.todoapp.global.auth.filter.VerifyUserFilter;
+import com.todo.todoapp.global.auth.resolver.AuthArgumentResolver;
 import com.todo.todoapp.infrastructure.jwt.JwtUtil;
 import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
-public class WebConfig {
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthArgumentResolver authArgumentResolver;
+
     @Bean
     public FilterRegistrationBean verifyUserFilter(ObjectMapper mapper, UserService userService) {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
@@ -37,5 +47,10 @@ public class WebConfig {
         filterRegistrationBean.setFilter(new JwtAuthorizationFilter(jwtUtil));
         filterRegistrationBean.setOrder(2);
         return filterRegistrationBean;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authArgumentResolver);
     }
 }
